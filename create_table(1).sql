8e42b1428e63e7ee38fc38d8170e3bf83d51afd5
slautina01 <92513422+slautina01@users.noreply.github.com>
2021-10-14 11:44:42 +0300

    Add files via upload

---
+++ create_table(1).sql
@@ -0,0 +1,8 @@
+CREATE TABLE `g01u00`.`g03u55_goods` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `g03u55_type` VARCHAR(45) NULL,
+  `g03u55_name` VARCHAR(45) NULL,
+  `g03u55_quantity` VARCHAR(45) NULL,
+  `g03u55_price` VARCHAR(45) NULL,
+  `g03u55_discount` FLOAT NULL,
+  PRIMARY KEY (`id`));
